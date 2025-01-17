e58997b654c5529183c5463b5c8fad8f10b9ffd9
sunchae <tjsco94@naver.com>
2025-01-17 10:40:16 +0900

    feat: signup, emailcheck

---
+++ src/main/java/kt/aivle/member/model/dto/EmailCheckRequestDTO.java
@@ -0,0 +1,12 @@
+package kt.aivle.member.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailCheckRequestDTO {
+    private String email;
+}
