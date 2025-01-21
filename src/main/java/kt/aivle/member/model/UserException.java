651048d77c5daad154f5bc28ff2e8c25cc61fa79
sunchae <tjsco94@naver.com>
2025-01-21 09:54:32 +0900

    model structure refactoring

--- src/main/java/kt/aivle/member/model/UserException.java
+++ src/main/java/kt/aivle/member/model/UserException.java
@@ -1,17 +1,17 @@
-package kt.aivle.member.model.dto;
+package kt.aivle.member.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ResponseDTO<T> {
+public class UserException<T> {
     private int resultCode; // 상태 코드
     private String resultMsg; // 메세지
     private T data; // 데이터
 
     // data 없이 사용할 수 있는 생성자 추가
-    public ResponseDTO(int resultCode, String resultMsg) {
+    public UserException(int resultCode, String resultMsg) {
         this.resultCode = resultCode;
         this.resultMsg = resultMsg;
         this.data = null;
--- src/main/java/kt/aivle/member/model/UserResponse.java
+++ src/main/java/kt/aivle/member/model/UserResponse.java
@@ -1,15 +1,15 @@
-package kt.aivle.member.model.dto;
+package kt.aivle.member.model;
 
 import lombok.Data;
 
 @Data
-public class SignupRequestDTO {
+public class UserResponse {
+    private Integer userSn;
+    private String password;
     private String userName;
     private String email;
-    private String password;
     private String gender;
     private String address;
-    private String zipCode;
     private String telno;
-
+    private String useYn;
 }
